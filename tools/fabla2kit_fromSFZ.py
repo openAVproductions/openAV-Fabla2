387615c476c501e6d78480c5f73fc3a6fe1bdca9
Harry van Haaren <harryhaaren@gmail.com>
2015-01-23 00:46:16 +0000

    -Initial commit of SFZ->Fabla2 kit converter

---
+++ tools/fabla2kit_fromSFZ.py
@@ -0,0 +1,152 @@
+
+# This is a very simple text file parser:
+# -which will auto-generate .html files
+# -from input HTML files
+# -and sort the pages into directories
+import os, sys, time
+import json
+
+pages = []
+
+
+def processFile( filename ):
+  # open the template & input file
+  sfz = open(filename)
+  
+  jsonData = '''{\n "bank_A":{"name":"AvLinux Red Zepplin 5pc",'''
+  
+  layer = ""
+  
+  boolFirstGroup = 1
+  boolFirstRegion = 1
+  
+  groupData = ""
+  
+  for l in sfz:
+    if l.startswith("//"):
+      # Comment: ignore
+      pass
+    
+    # handle <group> key=56 pan=-63 loop_mode=one_shot
+    if l.startswith("<group>"):
+      
+      if boolFirstGroup != 1:
+        groupData += ''',"nLayers": ''' + str(layer)
+        groupData += '},'
+      
+      
+      layer = 0
+      boolFirstRegion = 1
+      
+      
+      #print( "Processsing: " + l )
+      items = l.split(' ')
+      
+      key = -1
+      muteGroup = -1
+      offGroup = -1
+      
+      for i in items:
+        #print( i )
+        if i.startswith( 'key=' ):
+          key = i[4:]
+          pass
+        
+        if i.startswith( 'pan=' ):
+          #print( i[4:] )
+          pass
+        
+        if i.startswith( 'loop_mode=' ):
+          #print( i[10:] )
+          pass
+        
+        if i.startswith( 'group=' ):
+          muteGroup = i[6:]
+          pass
+        
+        if i.startswith( 'offby=' ):
+          offGroup = i[6:]
+          pass
+      
+      if int(key)-36 < 0:
+        print("WARNING: key out of range!" + str(items) )
+        time.sleep(3)
+      
+      if int(key) == 49:
+        print( i )
+      
+      # "pad_0": {
+      groupData += '''\n\n "pad_''' + str( int(key)-36 ) + '''" : \n''' + '{'
+      
+      groupData += '''"triggerMode": 1,\n'''
+      groupData += '''"volume": 0.75,\n'''
+      groupData += '''"switchMode": 2,\n'''
+      
+      if int(muteGroup) != -1:
+        groupData += '''"muteGroup": '''+ muteGroup +''',\n'''
+      
+      if int(offGroup) != -1:
+        groupData += '''"offGroup": '''+ offGroup +''',\n'''
+      
+      
+      boolFirstGroup = 0
+      
+      #print(groupData)
+      
+      
+      
+    
+    if l.startswith("<region>"):
+      if boolFirstRegion != 1:
+        groupData += ','
+      else:
+        boolFirstRegion = 0
+      
+      if key == 49:
+        print( l )
+      
+      items = l.split(' ')
+      filename = ""
+      lowVel   = 1
+      highVel  = 127
+      for i in items:
+        if i.startswith( 'lovel=' ):
+          lowVel = i[6:]
+        if i.startswith( 'hivel=' ):
+          highVel = i[6:]
+        if i.startswith( 'sample=' ):
+          filename = i[7:].rstrip()
+      
+      #print( "Low: " + str(lowVel) + "  High: " + str(highVel) + "  File: "+ filename )
+      
+      # "velHigh": 127,
+      # "velLow": 0
+      # "filename": "pad0_layer0.wav",
+      groupData += '''"layer_''' + str(layer) + '''":  '''
+      groupData += '{'
+      groupData += '''"filename": "''' + filename + '''",'''
+      groupData += '''"velHigh": ''' + str(highVel) + ''','''
+      groupData += '''"velLow": ''' + str(lowVel) + ''''''
+      groupData += '}\n'
+      
+      layer = layer + 1
+      
+  
+  jsonData += groupData
+  
+  groupData += '}'
+  
+  jsonData += '}'
+  
+  jsonData += '}' # bank
+  
+  jsonData += '}'
+  
+  #print( "FINAL:\n" + jsonData )
+  
+  outfile = open( "sfzExport_fabla2.json", 'w' );
+  outfile.write( jsonData )
+  
+  
+
+processFile( 'Red_Zeppelin_5pc.sfz' )
