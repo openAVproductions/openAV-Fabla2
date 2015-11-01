fe58249b7a24e2863a50152c0932357f1e3494c7
Harry van Haaren <harryhaaren@gmail.com>
2015-11-01 12:10:08 +0000

    Squashed 'src/ui/avtk/' changes from f85c030..e4cd636
    e4cd636 Fixed Widget Default value detail
    81a56ea Fixed widget default change issue
    e7bf4ef Fixed helpers issue
    af6941d Fixed warnings with helpers.hxx
    1290c34 Updated Dial text
    a5a3191 Updated git ignore
    91845e7 Merge branch 'master' of github.com:harryhaaren/openAV-AVTK
    300f056 -Implemented Dial label draw, UI BG_DARK by default
    1c7e26c Merge pull request #4 from fundamental/master
    2893885 Fix printf() Type Issues Reported By Clang
    8568781 Fix Gcc Build
    
    git-subtree-dir: src/ui/avtk
    git-subtree-split: e4cd636f82f9670af6833a0b0c50aeda9fd21d11

--- avtk/widget.cxx
+++ avtk/widget.cxx
@@ -125,10 +125,12 @@ int Widget::handle( const PuglEvent* event )
             {
               auditionValue_ = value_;
               value( defaultValue_ );
+              callback( this, callbackUD );
             }
             else
             {
               value( auditionValue_ );
+              callback( this, callbackUD );
             }
             
             AVTK_DEV("rclick - reset to default - value( %f )\n", defaultValue_ );
@@ -346,7 +348,9 @@ void Widget::value( float v )
 
 void Widget::defaultValue( float dv )
 {
+  // store new default value as both default and audition
   defaultValue_ = dv;
+  auditionValue_ = dv;
 }
 
 bool Widget::touches( int inx, int iny )
