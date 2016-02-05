238fe1997321ce414a1ef68a0ff7a394e5652f3f
Harry van Haaren <harryhaaren@gmail.com>
2016-02-05 20:58:09 +0000

    Squashed 'src/ui/avtk/' changes from 3a89e25..ab106da
    ab106da Fix prints, change default colour of avtkDemo back to blue
    d914cbd Refactored code with Astyle
    
    git-subtree-dir: src/ui/avtk
    git-subtree-split: ab106da07f29ca92bb3361cf3776b6709571bee9

--- avtk/theme.cxx
+++ avtk/theme.cxx
@@ -19,106 +19,97 @@ namespace Avtk
 int Theme::privateID = 0;
 
 Theme::Theme( Avtk::UI* ui_, std::string colour ) :
-  ui( ui_ ),
-  ID( privateID++ ),
-  cornerRadius_( 1 ),
-  lineWidthThin_( 0.9 ),
-  lineWidthNorm_( 1.1 ),
-  lineWidthWide_( 2.1 )
+	ui( ui_ ),
+	ID( privateID++ ),
+	cornerRadius_( 1 ),
+	lineWidthThin_( 0.9 ),
+	lineWidthNorm_( 1.1 ),
+	lineWidthWide_( 2.1 )
 {
-  int loadError = true;
+	int loadError = true;
 
-  loadError = load( colour );
+	loadError = load( colour );
 
-  if( loadError )
-  {
-    AVTK_DEV("Theme::Theme() Load error: using hard coded defaults\n" );
-    // set default values to the colors array
-    colors[BG].c[0] = colors[BG].c[1] = colors[BG].c[2] = 34;
-    colors[BG_DARK].c[0] = colors[BG_DARK].c[1] = colors[BG_DARK].c[2] = 17;
-    
-    colors[FG].c[0] = 76;
-    colors[FG].c[1] = 80;
-    colors[FG].c[2] = 83;
-    
-    colors[FG_DARK].c[0] = 35;
-    colors[FG_DARK].c[1] = 87;
-    colors[FG_DARK].c[2] =136;
-    
-    colors[HIGHLIGHT].c[0] =  0;
-    colors[HIGHLIGHT].c[1] =128;
-    colors[HIGHLIGHT].c[2] =255;
-  }
+	if( loadError ) {
+		AVTK_DEV("Theme::Theme() Load error: using hard coded defaults\n" );
+		// set default values to the colors array
+		colors[BG].c[0] = colors[BG].c[1] = colors[BG].c[2] = 34;
+		colors[BG_DARK].c[0] = colors[BG_DARK].c[1] = colors[BG_DARK].c[2] = 17;
+
+		colors[FG].c[0] = 76;
+		colors[FG].c[1] = 80;
+		colors[FG].c[2] = 83;
+
+		colors[FG_DARK].c[0] = 35;
+		colors[FG_DARK].c[1] = 87;
+		colors[FG_DARK].c[2] =136;
+
+		colors[HIGHLIGHT].c[0] =  0;
+		colors[HIGHLIGHT].c[1] =128;
+		colors[HIGHLIGHT].c[2] =255;
+	}
 }
 
 int Theme::load( std::string theme )
 {
-  //printf("%s : jsonTheme = %s\n", __FUNCTION__, theme.c_str() );
-  try
-  {
-    picojson::value v;
-    std::stringstream ifs;
-    ifs << theme;
-    ifs >> v;
-    
-    const char* items[5] = 
-    {
-      "bg",
-      "bg-dark",
-      "fg",
-      "fg-dark",
-      "highlight"
-    };
-    
-    for( int i = 0; i < 5; i++ )
-    {
-      // extract the 3 ints from the array, and store into Color array
-      int colNum = 0;
-      
-      if( !v.is<picojson::object>() )
-      {
-        printf("Error %s: v is NOT array\n", __PRETTY_FUNCTION__);
-        return -1;
-      }
-      
-	picojson::array list = v.get( items[i] ).get<picojson::array>();
-	//printf("array list ok\n");
+	try {
+		picojson::value v;
+		std::stringstream ifs;
+		ifs << theme;
+		ifs >> v;
 
-	//std::cerr << picojson::get_last_error() << std::endl;
+		const char* items[5] = {
+			"bg",
+			"bg-dark",
+			"fg",
+			"fg-dark",
+			"highlight"
+		};
 
-	for (picojson::array::iterator iter = list.begin();
-	     iter != list.end(); ++iter)
-	{
-	  double tmp = (int)(*iter).get("c").get<double>();
-	  //printf("%s = %lf\r\n", items[i], tmp );
-	  colors[i].c[colNum++] = tmp;
+		for( int i = 0; i < 5; i++ ) {
+			// extract the 3 ints from the array, and store into Color array
+			int colNum = 0;
+
+			if( !v.is<picojson::object>() ) {
+				printf("%s : Error  v is NOT array\n", __PRETTY_FUNCTION__);
+				return -1;
+			}
+
+			picojson::array list = v.get( items[i] ).get<picojson::array>();
+			//printf("array list ok\n");
+
+			//std::cerr << picojson::get_last_error() << std::endl;
+
+			for (picojson::array::iterator iter = list.begin();
+			     iter != list.end(); ++iter) {
+				double tmp = (int)(*iter).get("c").get<double>();
+				//printf("%s = %lf\r\n", items[i], tmp );
+				colors[i].c[colNum++] = tmp;
+			}
+		}
+	} catch( ... ) {
+		printf("Theme::load() Error loading theme from %s : falling back to"\
+		       "default.Double check file-exists and JSON contents valid.\n",
+		       theme.c_str() );
+		return -1;
 	}
-    }
-  }
-  catch( ... )
-  {
-    printf("Theme::load() Error loading theme from %s : falling back to"\
-	   "default.Double check file-exists and JSON contents valid.\n",
-	   theme.c_str() );
-    return -1;
-  }
-  return 0;
+	return 0;
 }
 
 void Theme::cornerRadius( int c )
 {
-  cornerRadius_ = c;
-  ui->redraw();
+	cornerRadius_ = c;
+	ui->redraw();
 }
 
 float Theme::color( cairo_t* cr, USE_CASE uc, float alpha_ )
 {
-  float r = colors[uc].c[0] / 255.;
-  float g = colors[uc].c[1] / 255.;
-  float b = colors[uc].c[2] / 255.;
-  //printf("%f, %f, %f\n", r, g, b );
-  cairo_set_source_rgba(cr, r, g, b, alpha_);
-  return 0;
+	float r = colors[uc].c[0] / 255.;
+	float g = colors[uc].c[1] / 255.;
+	float b = colors[uc].c[2] / 255.;
+	//printf("%f, %f, %f\n", r, g, b );
+	cairo_set_source_rgba(cr, r, g, b, alpha_);
+	return 0;
 }
 
 }; // Avtk
--- build/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-make
-
-#  --gen-suppressions=all
-valgrind --suppressions=valgrind.suppressions --leak-check=full ./avtk
