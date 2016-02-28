a32632cf136a4e4f9a9613b4720d0ef74de317e0
Harry van Haaren <harryhaaren@gmail.com>
2016-02-28 23:38:42 +0000

    ui: cleanup Step class

--- src/ui/step.hxx
+++ src/ui/step.hxx
@@ -11,11 +11,9 @@ class Step : public Button
 public:
 	Step( Avtk::UI* ui, int x, int y, int w, int h, std::string label);
 	virtual ~Step() {}
-	virtual void draw( cairo_t* cr );
 	int row;
 	int col;
 };
-
 };
 
 #endif // OPENAV_AVTK_STEP_HXX
